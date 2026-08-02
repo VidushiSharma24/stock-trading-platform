497ef406a8b72335002979a03985e8f07682cfa6
VidushiSharma24 <vidushisharma0204@gmail.com>
2026-08-02 13:56:43 +0530

    Add backend

---
+++ backend/model/HoldingsModel.js
@@ -0,0 +1,7 @@
+const { model } = require("mongoose");
+
+const { HoldingsSchema } = require("../Schemas/HoldingsSchema");
+
+const HoldingsModel = model("Holding", HoldingsSchema);
+
+module.exports = { HoldingsModel };
